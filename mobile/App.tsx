3ec8debdc944844158ba6903a1e57be2469444d3
Alessandro1918 <alessandro1918@hotmail.com>
2023-01-27 16:18:46 -0300

    feat: mobile screens and navigation

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,3 +1,5 @@
+import "./src/lib/dayjs"
+
 // import { StatusBar } from 'expo-status-bar';
 import { StatusBar } from 'react-native';
 import { 
@@ -9,7 +11,7 @@ import {
 } from '@expo-google-fonts/inter'
 
 import { Loading } from './src/components/Loading';
-import { Home } from './src/screens/Home';
+import { Routes } from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -25,7 +27,7 @@ export default function App() {
 
   return (
     <>
-      <Home />
+      <Routes />
       <StatusBar 
         barStyle="light-content" 
         backgroundColor='transparent' 
