cd48338e1ed52699ec07cb0a5d37286c63646178
Alessandro1918 <alessandro1918@hotmail.com>
2023-02-03 20:09:40 -0300

    refactor: checkbox made into separate component

--- front/src/components/DayHabits.tsx
+++ front/src/components/DayHabits.tsx
@@ -1,7 +1,6 @@
-import * as Checkbox from "@radix-ui/react-checkbox"
-import dayjs from "dayjs"
-import { Check } from "phosphor-react"
 import { useEffect, useState } from "react"
+import dayjs from "dayjs"
+import { Checkbox } from "./Checkbox"
 import { api } from "../lib/axios"
 
 interface DayHabitsProps {
@@ -65,27 +64,14 @@ export function DayHabits(props: DayHabitsProps) {
       {
         dayHabits?.possibleHabits.map(habit => {
           return (
-            //{/* tailwind trick: "group": allows me to style components based on attributes they don't have, but someone inside the group does */}
-            //{/* (style the parent "div" based on the state of the "Indicator" child) */}
-            <Checkbox.Root 
+            <Checkbox 
               key={habit.id}
-              className="flex items-center gap-3 group focus:outline-none disabled:cursor-not-allowed"
-              disabled={isDateNotToday}
+              title={habit.title}
               checked={dayHabits.completedHabits.includes(habit.id)}
-              onCheckedChange={() => handleToggleHabit(habit.id)}
-            >
-
-            {/* Here I style a div because an unchecked Radix checkbox does not get rendered */}
-            <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 transition-colors group-focus:ring-2 group-focus:ring-violet-600 group-focus:ring-offset-2 group-focus:ring-offset-background">
-              <Checkbox.Indicator>
-                <Check size={20} className="text-white"/>
-              </Checkbox.Indicator>
-            </div>
-
-            <span className="font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400">
-              {habit.title}
-            </span>
-          </Checkbox.Root>
+              disabled={isDateNotToday}
+              // onCheckedChange={() => handleToggleHabit(habit.id)}  //prop from Radix Checkbox
+              onChange={() => handleToggleHabit(habit.id)}            //prop from my custom component                     //prop from my custom component
+            />
           )
         })
       }
--- front/src/components/NewHabitForm.tsx
+++ front/src/components/NewHabitForm.tsx
@@ -1,6 +1,6 @@
-import * as Checkbox from "@radix-ui/react-checkbox"
-import { Check } from "phosphor-react";
 import { FormEvent, useState } from "react";
+import { Check } from "phosphor-react";
+import { Checkbox } from "./Checkbox"
 import { api } from "../lib/axios";
 
 const availableWeekDays = [
@@ -74,26 +74,14 @@ export function NewHabitForm() {
         {
           availableWeekDays.map((weekday, i) => {
             return (
-              //tailwind trick: "group": allows me to style components based on attributes they don't have, but someone inside the group does
-              //(style the parent "div" based on the state of the "Indicator" child)
-              <Checkbox.Root 
+              <Checkbox 
                 key={weekday}
-                className="flex items-center gap-3 group focus:outline-none"
-                onCheckedChange={() => handleToggleWeekDay(i)}
-                checked={weekDays.includes(i)}  //just to clear component after form submit; I already got it's value on the state with the onChange
-              >
-
-              {/* Here I style a div because an unchecked Radix checkbox does not get rendered */}
-              <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 transition-colors group-focus:ring-2 group-focus:ring-violet-600 group-focus:ring-offset-2 group-focus:ring-offset-background">
-                <Checkbox.Indicator>
-                  <Check size={20} className="text-white"/>
-                </Checkbox.Indicator>
-              </div>
-
-              <span className="text-white leading-tight">
-                {weekday}
-              </span>
-            </Checkbox.Root>
+                title={weekday}
+                checked={weekDays.includes(i)}                     //just to clear component after form submit; I already got it's value on the state with the onChange
+                // disabled  //not applicable
+                // onCheckedChange={() => handleToggleWeekDay(i)}  //prop from Radix Checkbox
+                onChange={() => handleToggleWeekDay(i)}            //prop from my custom component
+              />
             )
           })
         }
